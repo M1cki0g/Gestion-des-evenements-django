767d7932c08bfadf70edd2822304ab6bca6181bf
M1cki0g <yassinetouli35@gmail.com>
2025-04-04 22:51:08 +0000

    second update

--- gestion_des_evenements/models.py
+++ gestion_des_evenements/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+import datetime
+import os
 
-# Create your models here.
+# Helper function to generate a unique file path
+def file_path(instance, filename):
+    timenow = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    # Safe filename creation
+    filename = f"{timenow}_{filename}"
+    return os.path.join('uploads', filename)
+
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    description = models.TextField(null=True, max_length=500)
+    image = models.ImageField(upload_to=file_path, null=True, blank=True)
