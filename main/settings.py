3c183d380000be47218f8c743cb73ce23c27856e
M1cki0g <yassinetouli35@gmail.com>
2025-05-21 12:48:26 +0100

    update

--- main/settings.py
+++ main/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-from dotenv import load_dotenv
+# import dj_database_url
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 
 
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQLDATABASE'),
-        'USER': os.getenv('MYSQLUSER'),
-        'PASSWORD': os.getenv('MYSQLPASSWORD'),
-        'HOST': os.getenv('MYSQLHOST'),
-        'PORT': os.getenv('MYSQLPORT'),
+        'NAME': 'evenement',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
         # 'ENGINE': 'django.db.backends.sqlite3',
--- main/wsgi.py
+++ main/wsgi.py
@@ -12,9 +12,9 @@ from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application() 
 
 # app = application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
