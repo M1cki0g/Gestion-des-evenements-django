8cd3fc08e05d49213e06f4cca1513f3e1f6cb362
M1cki0g <yassinetouli35@gmail.com>
2025-05-22 23:04:51 +0100

    Update database configuration and deployment settings

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: DJANGO_SETTINGS_MODULE=main.production gunicorn main.wsgi --log-file -
--- main/production.py
+++ main/production.py
@@ -19,9 +19,19 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 # Database configuration
-DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
